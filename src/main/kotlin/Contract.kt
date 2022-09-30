e4b96ee933e3dcb324dd82b1192f39149964f9f4
Mike Sperber <sperber@deinprogramm.de>
2022-09-30 14:54:49 +0200

    Update.

--- src/main/kotlin/Contract.kt
+++ src/main/kotlin/Contract.kt
@@ -3,6 +3,11 @@
 Zero-Coupon Bond / Zero-Bond
 "Ich bekomme 100€ an Weihnachten."
 
+2. zerlegen in "atomare Bestandteile" / Ideen / Aspekte
+
+- Währung: "Ich bekomme 1€ jetzt."
+- Vielfaches: "Ich bekomme 100€ jetzt."
+- Später
  */
 
 
@@ -14,4 +19,11 @@ data class Date(val iso: String)
 
 val christmas = Date("2022-12-24")
 
-data class ZeroCouponBond(val amount: Amount, val currency: Currency, val date: Date)
+
+/* gescheitert:
+data class ZeroCouponBond(val amount: Amount, val currency: Currency, val date: Date): Contract
+data class Future(val foo: Int): Contract
+data class Call(val foo: Int): Contract
+ */
+
+sealed interface Contract
