ec798c83c5deefc3b31e7ecd8d7db9046ff0445b
Mike Sperber <sperber@deinprogramm.de>
2022-09-30 09:51:14 +0200

    Update.

--- src/main/kotlin/Animals.kt
+++ src/main/kotlin/Animals.kt
@@ -1,2 +1,18 @@
-class Animals {
-}
+// Tiere auf dem texanischen Highway
+
+// Datendefinition:
+// Gürteltier hat folgende Eigenschaften:
+// - tot oder lebendig - UND -
+// - Gewicht
+
+// "Lebendigkeit" ist eins der folgenden:
+// - tot - ODER -
+// - lebendig
+// Fallunterscheidung
+// hier speziell: Aufzählung
+
+enum class Liveness { DEAD, ALIVE }
+
+typealias Weight = Integer
+
+data class Dillo(val liveness: Liveness, val weight: Weight)
