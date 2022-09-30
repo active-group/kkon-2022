d4da7e2da5b266af6f8ff649678b7b21cc812cf5
Mike Sperber <sperber@deinprogramm.de>
2022-09-30 10:20:08 +0200

    Update.

--- src/main/kotlin/River.kt
+++ src/main/kotlin/River.kt
@@ -20,5 +20,16 @@ val schlichem = Creek("Schlichem", "Tieringen")
 // - Hauptfluss
 // - Nebenfluss
 data class Confluence(val name: String, val location: String,
-                      val mainStem: River, val tributary: River)
+                      val mainStem: River, val tributary: River): River
 //                                  ^^^^^                 ^^^^^
+//                                  Selbstbezug
+
+// Fließt Wasser aus einem Ort in einen Fluss?
+fun flowsFrom(location: String, river: River): Boolean =
+    when (river) {
+        is Creek -> location == river.origin
+        is Confluence ->
+            // Selbstbezug in der Eingabe => rekursiver Aufruf
+            flowsFrom(location, river.mainStem)
+            flowsFrom(location, river.tributary)
+    }
