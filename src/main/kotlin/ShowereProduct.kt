571407ed7289967c99e7c44e9658f4f0631a342d
Mike Sperber <sperber@deinprogramm.de>
2022-09-30 10:28:32 +0200

    Update.

--- src/main/kotlin/ShowereProduct.kt
+++ src/main/kotlin/ShowereProduct.kt
@@ -1,4 +1,6 @@
 // Ein Duschprodukt:
 // - Seife (pH-Wert)
 // - Shampoo (Haartyp)  (seifenfrei)
-// - Mixtur bestehend aus zwei beliebigen Duschprodukten
+// - Mixtur bestehend aus zwei beliebigen Duschprodukten, beliebiges Mischungsverhältnis
+
+// Wie hoch ist der Seifenanteil?
