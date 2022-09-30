7271195f78e988b74af5995afe026119dcad56f1
Mike Sperber <sperber@deinprogramm.de>
2022-09-30 11:04:51 +0200

    Update.

--- src/main/kotlin/List.kt
+++ src/main/kotlin/List.kt
@@ -1,1 +1,7 @@
-// 
+// Eine Liste ist eins der folgenden:
+// - die leere Liste
+// - eine Cons-Liste aus erstem Element und Rest-Liste
+//                                               ^^^^^ Selbstbezug
+sealed interface List<A>
+
+object Empty : List<Nothing>
