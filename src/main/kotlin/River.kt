5f7552a83bf00fb109df885c5d1afd5b074eb907
Mike Sperber <sperber@deinprogramm.de>
2022-09-30 10:14:38 +0200

    Update.

--- src/main/kotlin/River.kt
+++ src/main/kotlin/River.kt
@@ -13,3 +13,11 @@ data class Creek(val name: String, val origin: String): River
 val eschach = Creek("Eschach", "Heimliswald")
 val prim = Creek("Prim", "Dreifaltigkeitsberg")
 val schlichem = Creek("Schlichem", "Tieringen")
+
+// Ein Zusammenfluss hat folgende Eigenschaften:
+// - Name
+// - Ort des Zusammenflusses
+// - Hauptfluss
+// - Nebenfluss
+data class Confluence(val name: String, val location: String,
+                      val mainStem: River, val tributary: River)
