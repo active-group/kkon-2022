d6d27339749225798b250d207f3ca6ec61f64a4a
Mike Sperber <sperber@deinprogramm.de>
2022-09-30 15:50:36 +0200

    Update.

--- src/main/kotlin/Contract.kt
+++ src/main/kotlin/Contract.kt
@@ -68,6 +68,8 @@ data class Payment(val direction: Direction,
                    val date: Date, val amount: Amount, val currency: Currency) {
     fun reverse(): Payment =
         this.copy(direction = if (direction == Direction.LONG) Direction.SHORT else Direction.LONG)
+    fun scale(factor: Amount) =
+        this.copy(amount = this.amount * factor)
 }
 
 // Zahlungen bis zum Zeitpunkt now + Residualvertrag
