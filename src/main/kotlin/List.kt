ef98233b0460211126b5d179b0c4f6340a42ab55
Mike Sperber <sperber@deinprogramm.de>
2022-09-30 11:12:42 +0200

    Update.

--- src/main/kotlin/List.kt
+++ src/main/kotlin/List.kt
@@ -13,3 +13,8 @@ data class Cons<A>(val first: A, val rest: List<A>): List<A>
 
 // Liste mit 1 Element: 5
 val list1 = Cons(5, Empty)
+
+// Any / (Java: Object) - Supertyp aller (Referenz)typen
+// Nothing: Subtyp aller Typen (keine Werte dieses Typs)
+// List<out A>: Die Listenelemente können Typ A oder einen Subtyp von A
+// Kovarianz
