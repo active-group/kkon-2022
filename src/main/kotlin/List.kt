c16e7b07656b00d8f4f7c3b182bffc545d62940c
Mike Sperber <sperber@deinprogramm.de>
2022-09-30 12:46:26 +0200

    Update.

--- src/main/kotlin/List.kt
+++ src/main/kotlin/List.kt
@@ -53,7 +53,7 @@ fun listProduct(list: List<Int>): Int =
             list.first * listProduct(list.rest)
     }
 
-fun listFold(e: Int, f: (Int, Int) -> Int, list: List<Int>): Int =
+fun <A> listFold(e: Int, f: (A, Int) -> Int, list: List<A>): Int =
     when (list) {
         is Empty -> e
         is Cons ->
