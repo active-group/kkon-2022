15d904ef82c0a7c20e51c761f6cd76f784f7155b
Mike Sperber <sperber@deinprogramm.de>
2022-09-30 14:49:10 +0200

    Update.

--- src/main/kotlin/Contract.kt
+++ src/main/kotlin/Contract.kt
@@ -4,3 +4,14 @@ Zero-Coupon Bond / Zero-Bond
 "Ich bekomme 100€ an Weihnachten."
 
  */
+
+
+typealias Amount = Double
+
+enum class Currency { EUR, GBP, CHF, USD, YEN }
+
+data class Date(val iso: String)
+
+val christmas = Date("2022-12-24")
+
+data class ZeroCouponBond(val amount: Amount, val currency: Currency, val date: Date)
